7d8b3c02525c22d70afc18bbe53df6bd541a410f
Wemmons831 <69495081+Wemmons831@users.noreply.github.com>
2023-02-13 09:09:44 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,7 @@ export default function Home() {
     </header>
     <main >
       <div className=' text-center'>
-        <text className=' text-center text-6xl'>GPT-GRADER</text>
+        <text className=' text-center text-6xl'>GPT-GRADER test</text>
       </div>
       
       <div className='  text-center pt-20'>
